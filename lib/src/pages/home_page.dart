28c5d353ba1e75eddf225d4c027265c4d76ae911
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-11-04 22:13:22 -0500

    Creating BottomNavigationBar with 2 Tabs: Maps and Addreses, and making logic to work them.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:qrscanner_sqlite_flutter/src/pages/addresses_page.dart';
+import 'package:qrscanner_sqlite_flutter/src/pages/maps_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
+      body: _callPage(_currentPage),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentPage,
+      onTap: (index){
+        setState(() {
+         _currentPage = index; 
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Maps')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.navigation),
+          title: Text('Addresses')
+        )
+      ],
+    );
+  }
+
+  Widget _callPage(int currentPage) {
+    switch(currentPage){
+      case 0: return MapsPage();
+      case 1: return AddressesPage();
+      default:
+        return MapsPage();
+    }
   }
 }
